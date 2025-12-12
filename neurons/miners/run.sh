525084bc5cdc7759092b1e0ed3108dd481d43779
pyon12 <pyon123@proton.me>
2025-12-12 16:39:33 +0000

    feat(miner): DAH-1642 - remove validator hotkey migration

--- neurons/miners/run.sh
+++ neurons/miners/run.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# db migrate (skip in central mode)
-# Convert CENTRAL_MODE to lowercase for case-insensitive comparison
-CENTRAL_MODE_LOWER=$(echo "$CENTRAL_MODE" | tr '[:upper:]' '[:lower:]')
-if [ "$CENTRAL_MODE_LOWER" != "true" ]; then
-  pdm run alembic upgrade head
-  # migrate validator hotkey
-  pdm run src/cli.py migrate-validator-hotkey
-fi
+# db migrate
+pdm run alembic upgrade head
 
 # run fastapi app
 pdm run src/miner.py
