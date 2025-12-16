2764ba4f3d8ed9394afe01499462c93c1bac0608
Marouane Elaich <elaich@gmx.net>
2025-12-16 17:20:29 +0800

    feat(validator): Allow a dry run mode to be able to run a second validator against real executors

--- neurons/validators/src/core/config.py
+++ neurons/validators/src/core/config.py
@@ -87,6 +87,7 @@ class Settings(BaseSettings):
     ENABLE_NO_COLLATERAL: bool = True
     ENABLE_VERIFYX: bool = True
     SKIP_RENTAL_VERIFICATION: bool = Field(env="SKIP_RENTAL_VERIFICATION", default=False)
+    DRY_RUN: bool = Field(env="DRY_RUN", default=False, description="Run validation without publishing scores/weights")
 
     COLLATERAL_CONTRACT_ADDRESS: str = Field(
         env='COLLATERAL_CONTRACT_ADDRESS', default='0x8A4023FdD1eaA7b242F3723a7d096B6CC693c7C6'
