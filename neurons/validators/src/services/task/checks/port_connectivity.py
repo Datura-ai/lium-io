f2b731330ce9e85eacbdc2ab59573a09d6139745
Mikhail Derbichev <arhangel662@gmail.com>
2025-12-30 19:46:20 +0400

    DAH-1689, don't check renting in progress in port_connectivity.

--- neurons/validators/src/services/task/checks/port_connectivity.py
+++ neurons/validators/src/services/task/checks/port_connectivity.py
@@ -18,22 +18,7 @@ class PortConnectivityCheck:
     fatal = False
 
     async def run(self, ctx: Context) -> CheckResult:
-        redis_service = ctx.services.redis
-        renting_in_progress = await redis_service.renting_in_progress(ctx.miner_hotkey, ctx.executor.uuid)
-        extra = {**ctx.default_extra, "renting_in_progress": renting_in_progress}
-
-        if renting_in_progress:
-            event = render_message(
-                Msg.RENTING_IN_PROGRESS,
-                ctx=ctx,
-                check_id=self.check_id,
-                what={"renting_in_progress": True},
-            )
-            return CheckResult(
-                passed=True,
-                event=event,
-                updates={"default_extra": extra, "renting_in_progress": True},
-            )
+        extra = {**ctx.default_extra}
 
         if not ctx.config.job_batch_id:
             event = render_message(
