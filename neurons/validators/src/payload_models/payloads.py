e9e277faf7fef22f5f5ca7e25d50fe72c74be86a
Mikhail Derbichev <arhangel662@gmail.com>
2026-01-02 18:24:13 +0400

    DAH-1689, pass ports from db

--- neurons/validators/src/payload_models/payloads.py
+++ neurons/validators/src/payload_models/payloads.py
@@ -13,6 +13,9 @@ class CustomOptions(BaseModel):
     startup_commands: str | None = None
     shm_size: str | None = None
     initial_port_count: int | None = None
+    docker_port_pairs: list[tuple[int, int]] | None = None  # [(internal, docker), ...]
+    jupyter_port_pair: tuple[int, int] | None = None
+
 
     @classmethod
     def sanitize(cls, custom_options: 'CustomOptions | None') -> 'CustomOptions':
@@ -40,6 +43,8 @@ class CustomOptions(BaseModel):
             initial_port_count=custom_options.initial_port_count,
             startup_commands=custom_options.startup_commands,
             shm_size=shm_size,
+            docker_port_pairs=custom_options.docker_port_pairs,
+            jupyter_port_pair=custom_options.jupyter_port_pair
         )
 
     @staticmethod
@@ -345,11 +350,11 @@ class ContainerCreated(ContainerBaseResponse):
     message_type: ContainerResponseType = ContainerResponseType.ContainerCreated
     container_name: str
     volume_name: str
-    port_maps: list[tuple[int, int]]
     profilers: list[dict] = []
     backup_log_id: str | None = None
     restore_path: str | None = None
     jupyter_url: str | None = None
+    port_maps: list[tuple[int, int]] = []
     warnings: list[ContainerWarningCode] | None = None
     storage_limit_gb: int | None = None
     volume_limit_gb: int | None = None
