029ddfd6b1cecb3d111b90ae68241afae4ee7a14
fortunelucky777 <waris980609@gmail.com>
2026-02-18 12:10:43 -0500

    fix(watchtower): mount .env file issue

--- watchtower/src/watchtower.py
+++ watchtower/src/watchtower.py
@@ -193,7 +193,7 @@ def pull_and_restart_containers(client: docker.DockerClient, image_name: str, re
                 restart_policy={"Name": "unless-stopped"},
                 volumes={
                     "/var/run/docker.sock": {"bind": "/var/run/docker.sock", "mode": "rw"},
-                    "/app/.env": {"bind": "/root/executor/.env", "mode": "rw"}
+                    settings.WATCHTOWER_ENV_FILE_PATH: {"bind": "/root/executor/.env", "mode": "rw"}
                 },
             )
 
