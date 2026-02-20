d6ff9baaf94d7baa4efcb835a61ecc3aa9ac7b72
fortunelucky777 <waris980609@gmail.com>
2026-02-20 10:08:39 -0500

    feat(watchtower): embed hotkey & external endpoint into docker image

--- watchtower/src/config.py
+++ watchtower/src/config.py
@@ -1,6 +1,14 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+WATCHTOWER_ENDPOINT_URL: str = "https://lium.io/api/watchtower/digest"
+WATCHTOWER_VALIDATOR_HOTKEY: str = "5F7X5UpKSr26KU3jKfpLmT8kuKtBNyHhEnfS8xtxPCqCb13p"
+
+try:
+    from config_override import WATCHTOWER_ENDPOINT_URL, WATCHTOWER_VALIDATOR_HOTKEY  # noqa: F401, F811
+except ImportError:
+    pass
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
@@ -8,8 +16,6 @@ class Settings(BaseSettings):
     WATCHTOWER_ENABLED: bool = Field(env="WATCHTOWER_ENABLED", default=True)
     WATCHTOWER_IMAGE: str = Field(env="WATCHTOWER_IMAGE", default="daturaai/compute-subnet-executor-runner")
     WATCHTOWER_INTERVAL: int = Field(env="WATCHTOWER_INTERVAL", default=300)
-    WATCHTOWER_ENDPOINT_URL: str = Field(env="WATCHTOWER_ENDPOINT_URL")
-    WATCHTOWER_VALIDATOR_HOTKEY: str = Field(env="WATCHTOWER_VALIDATOR_HOTKEY")
     WATCHTOWER_ENV_FILE_PATH: str = Field(env="WATCHTOWER_ENV_FILE_PATH", default="~/.env")
 
 
