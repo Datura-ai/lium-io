745f4b2f65add31b1db91d65d60329b1fd6d7779
Marouane Elaich <elaich@gmx.net>
2026-03-06 11:01:17 +0800

    Removed PortMappingDao code

--- neurons/validators/src/core/validator.py
+++ neurons/validators/src/core/validator.py
@@ -2,7 +2,6 @@ import asyncio
 import json
 import os
 
-from daos.port_mapping_dao import PortMappingDao
 from payload_models.payloads import MinerJobRequestPayload
 from clients.backend_client import BackendClient
 
@@ -18,7 +17,7 @@ from services.executor_connectivity.port_selector import PortSelector
 from services.executor_connectivity.port_tester import PortTester
 from services.executor_connectivity.port_verifiers import BatchVerifier, FallbackVerifier
 from services.executor_connectivity.orchestrator import ConnectivityOrchestrator
-from services.executor_connectivity.persister import PortResultPersister
+from services.executor_connectivity import ContainerCleanupService
 from services.executor_connectivity_service import ExecutorConnectivityService
 from services.file_encrypt_service import FileEncryptService
 from services.miner_service import MinerService
@@ -62,7 +61,6 @@ class Validator:
         self.verifyx_validation_service = VerifyXValidationService()
         self.collateral_contract_service = CollateralContractService()
         self.attestation_service = AttestationService()
-        self.port_mapping_dao = PortMappingDao()
 
         # Backend client for API requests
         keypair = settings.get_bittensor_wallet().get_hotkey()
@@ -82,7 +80,7 @@ class Validator:
                 ),
                 DindProbe(DindVerifier(ssh_service)),
             ),
-            persister=PortResultPersister(self.port_mapping_dao),
+            cleanup_service=ContainerCleanupService(),
         )
 
         task_service = TaskService(
@@ -92,21 +90,18 @@ class Validator:
             verifyx_validation_service=self.verifyx_validation_service,
             collateral_contract_service=self.collateral_contract_service,
             executor_connectivity_service=self.executor_connectivity_service,
-            port_mapping_dao=self.port_mapping_dao,
             backend_client=self.backend_client,
             attestation_service=self.attestation_service,
         )
         self.docker_service = DockerService(
             ssh_service=ssh_service,
             redis_service=self.redis_service,
-            port_mapping_dao=self.port_mapping_dao,
             attestation_service=self.attestation_service,
         )
         self.miner_service = MinerService(
             ssh_service=ssh_service,
             task_service=task_service,
             redis_service=self.redis_service,
-            port_mapping_dao=self.port_mapping_dao,
             attestation_service=self.attestation_service,
         )
 
